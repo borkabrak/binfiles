64fd16d4d65fd65ebba32f5bba85d78138b88cbd
Jonathan Carter <borkabrak@gmail.com>
2015-06-28 20:45:20 -0500

    Remove a webpage that doesn't really belong here

--- Mike's World-O-Programming - The Y Combinator (Slight Return)_files/ajs(1).php
+++
@@ -1,8 +0,0 @@
-(function(){
-var s = "\n";
-s += "<"+"div id=\'beacon_160c569241\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
-s += "<"+"img src=\'http://cas.ny.us.criteo.com/delivery/lg.php?ckmode=9&cb=160c569241&source=&loc=http%3a%2f%2fmvanier.livejournal.com%2f2897.html&c1=oUSZAktCdY3o2H5gWcom69jaYy6x2XMR45TXyU2VcYsPMMDz2wssf9sy0-OUA4Lmiwnm6kO9XF6pyzMREZcG3g_pUsy80oWRDK7IAZfGtZuOuuGUJXMDtLxhaflW2VeDo4iPzujJQQcJXAnFRaw8DGa45Yt0tYKwAPWPd7107aB2zeJRPtQkFPnJMKrPSQ2V&c2=VtXMEwGkGXCyvpDOxEpDynguBgM8azAf\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' />\n";
-s += "<"+"img width=\"0\" height=\"0\" src=\"http://sync.mathtag.com/sync/img?type=sync&mt_exid=20&redir=http%3a%2f%2fdis.ny.us.criteo.com%2frex%2fmatch.aspx%3fc%3d2%26uid%3d%5bMM_UUID%5d\">\n";
-s += "<"+"/div>\n";
-s += "\n";
-document.write(s);})();
--- Mike's World-O-Programming - The Y Combinator (Slight Return)_files/ajs.php
+++
@@ -1,8 +0,0 @@
-(function(){
-var s = "\n";
-s += "<"+"div id=\'beacon_4488efcd5c\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
-s += "<"+"img src=\'http://cas.ny.us.criteo.com/delivery/lg.php?ckmode=9&cb=4488efcd5c&source=&loc=http%3a%2f%2fmvanier.livejournal.com%2f2897.html&c1=oUSZAktCdY3o2H5gWcom69jaYy6x2XMRS-I92PwlIrmkMddjpajOX7fU3omMKcg15SrCdZaDoMuruoBkNna3EFAds1FOpDr_edAfz0DVJXwJEbEOvsGZd46WEZ-LjDOeG9EO0732iDGVq4jRHfLBFnfjUe0v9oB2PuzGJOAZSnZcBHdvnKfGEnl0zcG7Jm-p&c2=VtXMEwGkGXCyvpDOxEpDynguBgM8azAf\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' />\n";
-s += "<"+"img width=\"0\" height=\"0\" src=\"http://sync.mathtag.com/sync/img?type=sync&mt_exid=20&redir=http%3a%2f%2fdis.ny.us.criteo.com%2frex%2fmatch.aspx%3fc%3d2%26uid%3d%5bMM_UUID%5d\">\n";
-s += "<"+"/div>\n";
-s += "\n";
-document.write(s);})();
